c70d18ffc895fdc16ee9f437065b371c8675d683
agottipati9 <44129794+agottipati9@users.noreply.github.com>
2019-05-18 18:27:39 -0600

    Set up intial project for introduction to C#

---
+++ Projects/PS1 Simple Calculator/PS1 Simple Calculator/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PS1_Simple_Calculator
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+        }
+
+    }
+    /*
+     * Derivatives, simple integrals, etc, make this as complex as you want and have fun and test yourself :)
+     */
+
+    class Calculator
+    {
+        public Calculator()
+        {
+
+        }
+
+    }
+}
