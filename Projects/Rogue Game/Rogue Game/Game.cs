334676565db3d4ed8189d7270530b00d577c4b5b
agottipati9 <44129794+agottipati9@users.noreply.github.com>
2019-05-22 23:55:04 -0600

    Learned how to create a color palette. Created a color palette and tested it on the console.

--- Projects/Rogue Game/Rogue Game/Game.cs
+++ Projects/Rogue Game/Rogue Game/Game.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using RLNET;
+using Rogue_Game.Core;
 
 namespace Rogue_Game
 {
@@ -62,17 +63,17 @@ namespace Rogue_Game
         {
             //Set background color and text for each console
             // so that we can verify they are in the correct positions
-            _mapConsole.SetBackColor(0, 0, _mapWidth, _mapHeight, RLColor.Black);
-            _mapConsole.Print(1, 1, "Map", RLColor.White);
+            _mapConsole.SetBackColor(0, 0, _mapWidth, _mapHeight, Colors.FloorBackground);
+            _mapConsole.Print(1, 1, "Map", Colors.TextHeading);
 
-            _inventoryConsole.SetBackColor(0, 0, _inventoryWidth, _inventoryHeight, RLColor.Cyan);
-            _inventoryConsole.Print(1, 1, "Inventory", RLColor.White);
+            _inventoryConsole.SetBackColor(0, 0, _inventoryWidth, _inventoryHeight, Swatch.DbDeepWater);
+            _inventoryConsole.Print(1, 1, "Inventory", Colors.TextHeading);
 
-            _statConsole.SetBackColor(0, 0, _statWidth, _statHeight, RLColor.Brown);
-            _statConsole.Print(1, 1, "Stats", RLColor.White);
+            _statConsole.SetBackColor(0, 0, _statWidth, _statHeight, Swatch.DbWood);
+            _statConsole.Print(1, 1, "Stats", Colors.TextHeading);
 
-            _messageConsole.SetBackColor(0, 0, _messageWidth, _messageHeight, RLColor.Gray);
-            _messageConsole.Print(1, 1, "Messages", RLColor.White);
+            _messageConsole.SetBackColor(0, 0, _messageWidth, _messageHeight, Swatch.DbOldStone);
+            _messageConsole.Print(1, 1, "Messages", Colors.TextHeading);
         }
 
         //Event handler for RLNET's Render Event
